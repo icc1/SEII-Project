429b1a9c1e9bdcee1102313e027bd4a6e03997ae
Jakobi <jgs14@students.uwf.edu>
2017-03-13 23:41:18 -0500

    Combined the projects to just 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    private Animator animate;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animate = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
+        animate.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        animate.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
+    }
+}
