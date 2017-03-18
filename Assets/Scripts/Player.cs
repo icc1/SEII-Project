35eaead789637925a4941be1e5ca32ec2a09e02f
icc1 <icc1@students.uwf.edu>
2017-03-18 17:42:33 -0500

    Started Adding Classes and Interfaces
    Started adding classes and interfaces related to enemy encounters,
    enemies, enemy groups and the player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Player : IPlayer
+    {
+        private int attack;
+        private int health;
+        private int defense;
+        private int speed;
+
+        public int Attack
+        {
+            get
+            {
+                return attack;
+            }
+            set
+            {
+                attack = value;
+            }
+        }
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health = value;
+            }
+        }
+        public int Defense
+        {
+            get
+            {
+                return defense;
+            }
+            set
+            {
+                defense = value;
+            }
+        }
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                speed = value;
+            }
+        }
+
+        public void setStats(int newAttack, int newHealth, int newDefense, int newSpeed)
+        {
+            this.Attack = newAttack;
+            this.Health = newHealth;
+            this.Defense = newDefense;
+            this.Speed = newSpeed;
+        }
+
+    }
+}
