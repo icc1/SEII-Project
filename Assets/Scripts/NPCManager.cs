b7e8305dd958cd51f573a4df5856671dddb84105
Crystal Stewart <cfs2@students.uwf.edu>
2017-03-20 22:07:02 -0500

    Experimenting with NPC rendering
    Added and parsed out Cat Sprite. Added boundries.

--- Assets/Scripts/NPCManager.cs
+++ Assets/Scripts/NPCManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestingScriptDeletePlz : MonoBehaviour {
+public class NPCManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
