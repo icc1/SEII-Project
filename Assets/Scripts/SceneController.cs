324df5befa9ffa32231ec11f8e87dde91b8c3d85
Crystal Stewart <cfs2@students.uwf.edu>
2017-03-23 22:19:03 -0500

    Added Collision detection to load a new scene
    Added Collision detection to load a new scene when the player collides
    with the stairs. It loads another scene.

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneController: MonoBehaviour {
+    
+    void onTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Triggered.");
+        if (collider.name == "player")
+        {
+
+          //  gameObject.GetComponent<Renderer>().inbounds = true;
+            //alternatly store that someone is in the bounds in a variable
+            //in this script depending on which makes sense, for the player to
+            //know or the object
+        }
+    }
+        void OnCollisionEnter2D(Collision2D other)
+        {
+        SceneManager.LoadScene("Scenes/main", LoadSceneMode.Additive);
+        }
+    }
