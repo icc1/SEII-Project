c31985ea0e16f59f03d4625a6224893ad22da738
paolohora <pvh7@students.uwf.edu>
2017-03-24 20:07:47 -0500

    Start menu implemented and Improved Pause Menu
    Initial start menu has been implemented in which the user will be
    greeted before playing the game. Pause Menu is improved and player
    position will be saved. Player cannot move during pause.

---
+++ Assets/Scripts/WorkInProgress/ExitGameInGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameInGame : MonoBehaviour {
+
+	public void exit(bool selected)
+	{
+		if (selected == true) {
+			Application.Quit ();
+		}
+	}
+}
