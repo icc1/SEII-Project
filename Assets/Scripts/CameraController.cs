8db2db4baed4e90782900d9cf1819a8b5457d741
Jakobi <jgs14@students.uwf.edu>
2017-04-02 23:18:06 -0500

    Added Scene transition to the dungeon, script can be used anywhere.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,14 +7,25 @@ public class CameraController : MonoBehaviour {
     public GameObject targetToFollow;
     private Vector3 targetPosition;
     public float moveSpeed;
+    private static bool cameraExists;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        if (!cameraExists)
+        {
+            cameraExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         targetPosition = new Vector3(targetToFollow.transform.position.x, targetToFollow.transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
