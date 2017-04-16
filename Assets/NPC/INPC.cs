47d0eda59d2f2a6565dbebc99f0185d965259b12
Crystal Stewart <cfs2@students.uwf.edu>
2017-04-16 12:20:30 -0500

    NPC class and interface
    Created an NPC Class and interface to be used for generic NPCs. Only two
    classes created here, no other changes.

---
+++ Assets/NPC/INPC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Combat_Interfaces
+{
+    interface INPC : IStats
+    {
+        String NPCName { get; set; }
+        Location.Type NPCSpawnLocation { get; set; }
+        String NPCSprite { get; set; }
+    }
+}
---
+++ Assets/NPC/NPC.cs
@@ -0,0 +1,61 @@
+﻿using Assets.Scripts.Combat_Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class NPC : Stats, INPC
+    {
+        private String npcName;
+        private String npcSprite;
+        private Location.Type npcSpawnLocation;
+
+        public NPC(String NPCName, String NPCSprite, Location.Type NPCSpawnLocation,
+            int NPCAttack, int NPCHealth, int NPCDefense, int NPCSpeed)
+        {
+            this.NPCName = NPCName;
+            this.NPCSprite = NPCSprite;
+            this.NPCSpawnLocation = NPCSpawnLocation;
+            setStats(NPCAttack, NPCHealth, NPCDefense, NPCSpeed);
+        }
+        public String NPCName
+        {
+            get
+            {
+                return npcName;
+            }
+
+            set
+            {
+                npcName = value;
+            }
+        }
+        public Location.Type NPCSpawnLocation
+        {
+            get
+            {
+                return npcSpawnLocation;
+            }
+
+            set
+            {
+                npcSpawnLocation = value;
+            }
+        }
+        public String NPCSprite
+        {
+            get
+            {
+                return npcSprite;
+            }
+
+            set
+            {
+                npcSprite = value;
+            }
+        }
+    }
+}
