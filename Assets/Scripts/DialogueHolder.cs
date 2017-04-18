41ab55d19c22d82fd3d0debb91ce8d02840abd9d
Jakobi <jgs14@students.uwf.edu>
2017-04-18 17:45:18 -0500

    Added dialogue and text boxes

---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+    public string dialogue;
+    private DialogueManager diaManage;
+
+	// Use this for initialization
+	void Start () {
+        diaManage = FindObjectOfType<DialogueManager>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.gameObject.name == "Player")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                diaManage.ShowBox(dialogue);
+            }
+        }
+    }
+}
